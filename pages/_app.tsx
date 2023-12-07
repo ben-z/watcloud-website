dd0bc5942273815cfa146c423bebab211b631185
Ben Zhang <ben-z@users.noreply.github.com>
2023-12-06 17:22:09 -0800

    Fix Javascript error in Chrome 109: `TypeError: e.hostnames.toSorted is not a function` (#1927)
    We use
    [Array.toSorted](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toSorted)
    but it's not available until Chrome 110. And [Next.js happen to have
    missing polyfills for this
    function](https://github.com/vercel/next.js/issues/58242).
    
    This PR adds the missing polyfills. We should upgrade Next.js once the
    fix is merged upstream.
    
    Fixes #1926
    
    Size before:
    
    ```
    Route (pages)                                       Size     First Load JS
    ┌ ○ / (2829 ms)                                     2.1 kB          244 kB
    ├   └ css/0110e3f8d60db952.css                      515 B
    ├   /_app                                           0 B             228 kB
    ├ ○ /[404](https://github.com/WATonomous/infra-config/actions/runs/7122074753/job/19392599367#step:9:405)                                            182 B           228 kB
    ├ ○ /blog (2905 ms)                                 2.15 kB         244 kB
    ├ ○ /blog/hello-world (2913 ms)                     618 B           232 kB
    ├ ○ /docs (2920 ms)                                 2.77 kB         234 kB
    ├ ○ /docs/compute-cluster (2828 ms)                 577 B           232 kB
    ├ ○ /docs/compute-cluster/getting-access (2885 ms)  1.5 kB          233 kB
    ├ ○ /docs/compute-cluster/ssh (2956 ms)             96.3 kB         358 kB
    ├ ○ /docs/platform-usage (2957 ms)                  1.52 kB         233 kB
    ├ ○ /get-involved (2824 ms)                         573 B           232 kB
    └ ○ /machines (2932 ms)                             12.1 kB         273 kB
    + First Load JS shared by all                       248 kB
      ├ chunks/framework-f570b424fd8349c2.js            45.2 kB
      ├ chunks/main-db3ab7489dfde444.js                 29.6 kB
      ├ chunks/pages/_app-95697930cd3c0cb7.js           150 kB
      ├ chunks/webpack-f3fb839fd02756a5.js              2.6 kB
      └ css/8a997d74c211faed.css                        20 kB
    ```
    
    
    Size after:
    ```
    Route (pages)                                       Size     First Load JS
    ┌ ○ / (2631 ms)                                     4.5 kB          249 kB
    ├   └ css/0110e3f8d60db952.css                      515 B
    ├   /_app                                           0 B             135 kB
    ├ ○ /404                                            184 B           135 kB
    ├ ○ /blog (2702 ms)                                 4.57 kB         249 kB
    ├ ○ /blog/hello-world (2687 ms)                     3.01 kB         237 kB
    ├ ○ /docs (2597 ms)                                 5.12 kB         239 kB
    ├ ○ /docs/compute-cluster (2618 ms)                 3.05 kB         237 kB
    ├ ○ /docs/compute-cluster/getting-access (2598 ms)  3.88 kB         237 kB
    ├ ○ /docs/compute-cluster/ssh (2798 ms)             98.9 kB         362 kB
    ├ ○ /docs/platform-usage (2687 ms)                  3.93 kB         237 kB
    ├ ○ /get-involved (2617 ms)                         3.04 kB         237 kB
    └ ○ /machines (2798 ms)                             14.5 kB         278 kB
    + First Load JS shared by all                       141 kB
      ├ chunks/framework-2645a99191cfc5e9.js            45.2 kB
      ├ chunks/main-cfc0f54f4eeff935.js                 29.7 kB
      ├ chunks/pages/_app-6ccd0ef7[412](https://github.com/WATonomous/infra-config/actions/runs/7122415637/job/19393506342#step:9:413)87d5d.js           57.7 kB
      ├ chunks/webpack-ce6b989f[427](https://github.com/WATonomous/infra-config/actions/runs/7122415637/job/19393506342#step:9:428)10522.js              2.63 kB
      └ css/62d05959d9e1c125.css                        5.74 kB
    ```

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+// This file cannot be _app.mdx because the polyfills below gets lost in mdx translation.
+
+// Custom polyfills not yet available in `next-core`:
+// https://github.com/vercel/next.js/issues/58242
+// https://nextjs.org/docs/architecture/supported-browsers#custom-polyfills
+import 'core-js/features/array/to-reversed'
+import 'core-js/features/array/to-spliced'
+import 'core-js/features/array/to-sorted'
+
 import '../styles/global.css';
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { useReportWebVitals } from 'next/web-vitals'
 import { GoogleAnalytics, event } from "nextjs-google-analytics";
 import { websiteConfig } from '@/lib/data'
 
-export function reportWebVitals({ id, name, label, value }) {
+function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric) {
     // Report web vitals to Google Analytics: https://nextjs.org/docs/advanced-features/measuring-performance
     event(name, {
         category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
@@ -12,7 +23,9 @@ export function reportWebVitals({ id, name, label, value }) {
     });
 }
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
+    useReportWebVitals(reportWebVitals)
+
     return (
         <>
             <GoogleAnalytics trackPageViews gaMeasurementId={websiteConfig.ga_measurement_id} />
