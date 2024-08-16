190411692041c6b25492126159e717dc7367bba1
Ben Zhang <ben-z@users.noreply.github.com>
2024-08-16 13:10:08 -0700

    Create "services" page on the website (#3020)
    We used to have a web page that describes everything in the onboarding
    form. However, it was removed in
    https://github.com/WATonomous/infra-config/pull/2259 due to the lack of
    maintainers. This PR attempts to bring it back because we have been
    running into [spam filter
    issues](https://discord.com/channels/478659303167885314/571386709233893406/1273843070131114046)
    with our onboarding emails that include a lot of links.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/affiliation-list.tsx
+++ components/affiliation-list.tsx
@@ -15,36 +15,38 @@ export function AffiliationList({
   affiliationInfo: AffiliationInfo;
 }) {
   return (
-    <Command className="my-6">
-      <CommandInput placeholder="Find your affiliation..." />
-      <CommandList className="h-80">
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Affiliations">
-          {affiliationInfo.affiliations
-            .filter((a) => !a.is_legacy)
-            .map((a) => (
-              <CommandItem
-                key={a.name}
-                className="aria-selected:bg-inherit aria-selected:text-accent-inherit"
-              >
-                {a.name}
-              </CommandItem>
-            ))}
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Legacy affiliations">
-          {affiliationInfo.affiliations
-            .filter((a) => a.is_legacy)
-            .map((a) => (
-              <CommandItem
-                className="aria-selected:bg-inherit aria-selected:text-accent-inherit"
-                key={a.name}
-              >
-                {a.name}
-              </CommandItem>
-            ))}
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    <div className="my-6 border rounded">
+      <Command>
+        <CommandInput placeholder="Find your affiliation..." />
+        <CommandList className="h-80">
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Affiliations">
+            {affiliationInfo.affiliations
+              .filter((a) => !a.is_legacy)
+              .map((a) => (
+                <CommandItem
+                  key={a.name}
+                  className="aria-selected:bg-inherit aria-selected:text-accent-inherit"
+                >
+                  {a.name}
+                </CommandItem>
+              ))}
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Legacy affiliations">
+            {affiliationInfo.affiliations
+              .filter((a) => a.is_legacy)
+              .map((a) => (
+                <CommandItem
+                  className="aria-selected:bg-inherit aria-selected:text-accent-inherit"
+                  key={a.name}
+                >
+                  {a.name}
+                </CommandItem>
+              ))}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </div>
   );
 }
